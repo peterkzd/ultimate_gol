9389744d0cf90102346698117fbda7579aeed666
peterkzd <peter.kydas@zendesk.com>
2022-01-12 16:42:27 +1100

    minor bugfix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the title', () => {
   render(<App />);
   const title = screen.getByText(/The Ultimate Game of Life/i);
   expect(title).toBeInTheDocument();
